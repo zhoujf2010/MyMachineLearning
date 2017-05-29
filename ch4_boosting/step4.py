7faba5657c3909ee8189065d761b0fe7cac66d76
zhoujf2010 <zhoujf2010@gmail.com>
2017-05-29 20:50:36 +0800

    adaboost

--- ch4_boosting/step4.py
+++ ch4_boosting/step4.py
@@ -11,94 +11,78 @@ Adboost实现
 '''
 
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
 
-def stumClassify(x, dime, threshVal, threshIneq):
+def stumClassify(x, threshVal, threshIneq):
     retArr = np.ones((np.shape(x)[0], 1))
     if threshIneq == 'lt':
-        retArr[x[:, dime] <= threshVal] = 1.0
+        retArr[x <= threshVal] = -1.0
     else:
-        retArr[x[:, dime] > threshVal] = -1.0
+        retArr[x > threshVal] = -1.0
     return retArr
 
+# 建立树桩(Stump)
 def buildStump(x, y, D):
-    m, n = np.shape(x)
-    numStep = 10.0
+    m = np.shape(x)[0]
     bestStump = {}
     bestClasEst = np.mat(np.zeros((m, 1)))
     minEror = np.Inf
-    for i in range(n):
-        rangeMin = x[:, i].min()
-        rangeMax = x[:, i].max()
-        stepSize = (rangeMax - rangeMin) / numStep
-        for j in range(-1, int(numStep) + 1):
-            for inequal in ['lt', 'gt']:
-                threshVal = (rangeMin + float(j) * stepSize)
-                predictedVals = stumClassify(x, i, threshVal, inequal)
-                errArr = np.mat(np.ones((m, 1)))
-                errArr[predictedVals == y] = 0
-                weightError = D.T * errArr
-                # print 'split: dim %d,thresh %.2f,thresh inequal: %s,the wieght error is %.3f' % (i, threshVal, inequal, weightError)
-                if weightError < minEror:
-                    minEror = weightError
-                    bestClasEst = predictedVals.copy()
-                    bestStump['dim'] = i
-                    bestStump['thresh'] = threshVal
-                    bestStump['ineq'] = inequal
-    return bestStump, minEror, bestClasEst
+    for threshVal in np.arange(0.5, 9.5, 1):
+        for inequal in ['lt', 'gt']:
+            predictedVals = stumClassify(x, threshVal, inequal)
+            errArr = np.mat(np.ones((m, 1)))
+            errArr[predictedVals == y] = 0
+            weightError = D.T * errArr
+            if weightError < minEror:
+                minEror = weightError
+                bestClasEst = predictedVals.copy()
+                bestStump['thresh'] = threshVal
+                bestStump['ineq'] = inequal
+    return bestStump, minEror, bestClasEst.T
                     
-def adaBoostTrainDS(x, y, numIt=40):
+def adaBoostfit(x, y, numIt=40):
     weekClassArr = []
     m = np.shape(x)[0]
     D = np.mat(np.ones((m, 1)) / m)
     aggClassEst = np.mat(np.zeros((m, 1)))
-    for i in range(numIt):
+    for k in range(numIt):
         bestStump, error, classEst = buildStump(x, y, D)
-        print "D:", D.T
-        # error = np.array(error)[0][0]
-        alpha = float(0.5 * np.log((1.0 - error) / max(error, 1e-16)))
-        bestStump['alpha'] = alpha
+        print "D%d:" % k, D.T
+        gamma = float(0.5 * np.log((1.0 - error) / max(error, 1e-16)))
+        bestStump['gamma'] = gamma
+        print "alpha:", bestStump["ineq"], bestStump["thresh"] , '   gamma值：', gamma
         weekClassArr.append(bestStump)
-        print 'classEst:', classEst.T
-        expon = np.multiply(-1 * alpha * y, classEst)
+        print '分类y：', classEst
+        expon = np.multiply(-1 * gamma * y, classEst.T)
         D = np.multiply(D, np.exp(expon))
         D = D / D.sum()
-        aggClassEst += alpha * classEst
-        print "aggClassEst:", aggClassEst.T
+        aggClassEst += gamma * classEst.T
+        
         aggErrors = np.multiply(np.sign(aggClassEst) != y, np.ones((m, 1)))
         errorRate = aggErrors.sum() / m
-        print "total Error:", errorRate, "\n"
+        print "错误率：", errorRate, "\n"
         if errorRate == 0.0:break
     return weekClassArr 
 
-def addClassify(x, classArr):
+def addboostpredict(x, classArr):
     m = np.shape(x)[0]
     aggClassEst = np.mat(np.zeros((m, 1)))
     for i in range(len(classArr)):
-        classEst = stumClassify(x, classArr[i]["dim"], classArr[i]["thresh"], classArr[i]["ineq"])
-        aggClassEst += classArr[i]["alpha"] * classEst
-        print aggClassEst
+        classEst = stumClassify(x, classArr[i]["thresh"], classArr[i]["ineq"])
+        aggClassEst += classArr[i]["gamma"] * classEst
     return np.sign(aggClassEst)
-        
-
+    
 if __name__ == '__main__':
     np.set_printoptions(linewidth=300)
-    data = np.matrix([[0, 1], [1, 1], [2, 1], [3, -1], [4, -1], [5, -1], [6, 1], [7, 1], [8, 1], [9, -1]])
-#     data = np.matrix([[1., 2.1, 1.0], [2., 1.1, 1.0], [1.3, 1., -1.0], [1., 1., -1.0], [2., 1., 1.0]])
+    
+    data = np.matrix([[0.0, 1.0], [1, 1.0], [2, 1.0], [3, -1.0], [4, -1.0], [5, -1.0], [6, 1.0], [7, 1.0], [8, 1.0], [9, -1.0]])
     x, y = np.split(data, (1,), axis=1)
     
-    model = DecisionTreeClassifier(criterion='entropy',max_depth=1) #entropy or gini
-    model.fit(x, y)
-    
-    print "y_hat=", model.predict(x)
-    
-    
-    
+    # test tree stump
     D = np.mat(np.ones((len(x), 1)) / len(x))
+    print "test Tree Stump:", buildStump(x, y, D), "\n"
     
-    # print buildStump(x, y, D)
-    classArr = adaBoostTrainDS(x, y, 9)
-    print classArr
-    
-    print addClassify(x,classArr)     
+    classArr = adaBoostfit(x, y, 9)
+
+    y_hat = addboostpredict(x, classArr)
+    print "y_hat=", y_hat.T     
             
