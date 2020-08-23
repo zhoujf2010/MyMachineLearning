acaebb0ff1caccf6c095350daee5faf62a4109c6
zhoujf2010 <zhoujf2010@gmail.com>
2020-08-23 15:20:46 +0800

    add

--- chd_cnn/step1.py
+++ chd_cnn/step1.py
@@ -5,6 +5,10 @@ Created on 2019年4月1日
 @author: zjf
 '''
 
+'''
+手势识别
+'''
+
 import matplotlib.pyplot as plt
 import math
 import numpy as np
@@ -40,6 +44,7 @@ def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
     
     return mini_batches
 
+
 if __name__ == '__main__':
     # load data
     train_dataset = h5py.File('datasets/train_signs.h5', "r")
@@ -65,88 +70,104 @@ if __name__ == '__main__':
     print ("train num: " + str(m))
     # test显示
 #     plt.imshow(X_train[6])
-#     print(Y_train[:,6])
+#     print(Y_train[6])
 #     plt.show()
-    
-    tf.reset_default_graph()
-    tf.set_random_seed(1)  # 为方便观察结果
     
     # 配置一些运行参数
     learning_rate = 0.009
     num_epochs = 100
+    num_steps = 20
     minibatch_size = 64
     seed = 3
     costs = []
     
-    # 设置占位符
-    X = tf.placeholder(tf.float32, [None, 64, 64, 3], "X")
-    Y = tf.placeholder(tf.float32, [None, 6], "Y")
-
-    # 定义参数
-    W1 = tf.get_variable("W1", [4, 4, 3, 8], initializer=tf.contrib.layers.xavier_initializer(seed=0))
-    W2 = tf.get_variable("W2", [2, 2, 8, 16], initializer=tf.contrib.layers.xavier_initializer(seed=0))
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=8, kernel_size=(4, 4), data_format="channels_last", padding='same', strides=(1, 1)
+                               , input_shape=(64, 64, 3)),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.MaxPooling2D(pool_size=(8, 8), strides=(8, 8) , data_format="channels_last", padding='same'),
+        tf.keras.layers.Conv2D(filters=16, kernel_size=(4, 4), padding='same' , data_format="channels_last", strides=(1, 1)),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.MaxPooling2D(pool_size=(4, 4), strides=(4, 4) , data_format="channels_last", padding='same'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(6),
+        ])
     
-    # 设置前馈网络
-    Z1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding="SAME")
-    A1 = tf.nn.relu(Z1)
-    P1 = tf.nn.max_pool(A1, ksize=[1, 8, 8, 1], strides=[1, 8, 8, 1], padding="SAME")
-    Z2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding="SAME")
-    A2 = tf.nn.relu(Z2)
-    P2 = tf.nn.max_pool(A2, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding="SAME")
-    P2 = tf.contrib.layers.flatten(P2)
-    Z3 = tf.contrib.layers.fully_connected(P2, 6, activation_fn=None)
+    model.summary()
     
-    # 计算成本函数
-    costJ = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=Y))
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.fit(x=X_train,y=Y_train,epochs=100,batch_size=minibatch_size)
     
-    # 定义优化器
-    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(costJ)
-    
-    
-    # 启动session 计算TF图
-    with tf.Session() as sess:
-        np.random.seed(1)
-        tf.set_random_seed(1)
-        # 初使化
-        init = tf.global_variables_initializer()
-        sess.run(init)
-        print("W1=", W1.eval()[1, 1, 1])  # 测试一下输出值
-        print("W2=", W2.eval()[1, 1, 1])
-        a = sess.run(Z3, {X: np.random.randn(2,64,64,3), Y: np.random.randn(2,6)})
-        print("Z3 = " + str(a))
-        a = sess.run(costJ, {X: np.random.randn(4,64,64,3), Y: np.random.randn(4,6)})
-        print("cost = " + str(a))
-    
-        for epoch in range(num_epochs):  # 循环迭代次数
-            batchcost = 0
-            seed = seed + 1
-            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
-            # 分批计算
-            for batch in minibatches:
-                (batch_X, batch_Y) = batch
-                _, cost = sess.run([optimizer, costJ], feed_dict={X:batch_X, Y:batch_Y})
-                batchcost += cost / int(m / minibatch_size)
-             
-            if epoch % 5 == 0:
-                print ("Cost after epoch %i: %f" % (epoch, batchcost))
-            costs.append(batchcost)   
-    
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('cost')
-    plt.xlabel('iterations (per tens)')
-    plt.title("Learning rate =" + str(learning_rate))
-    plt.show()
- 
-    # Calculate the correct predictions
-    predict_op = tf.argmax(Z3, 1)
-    correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
- 
-    # Calculate accuracy on the test set
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print(accuracy)
-    train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
-    test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
-    print("Train Accuracy:", train_accuracy)
-    print("Test Accuracy:", test_accuracy)
+#     # 定义反向传播的优化器
+#     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+#      
+#     # 模型训练
+#     for i in range(num_steps):
+#         seed = seed + 1
+#         minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+#          
+#         batchcost = 0
+#             
+#             
+#         with tf.GradientTape() as tape:
+#             # 分批计算
+#             for batch in minibatches:
+#                 (batch_X, batch_Y) = batch
+#                 y_pred = model(batch_X)
+#                 # 定义损失函数
+#                 loss = tf.reduce_mean(tf.square(y_pred - batch_Y))
+#                 batchcost += loss / int(m / minibatch_size)
+#         if i % 2 == 0:
+#             print("batch %d: loss %f" % (i, loss.numpy()))
+#         grads = tape.gradient(loss, model.variables)
+#         optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
         
+    preds = model.evaluate(x=X_train,y=Y_train)
+    print("Train_Accuracy : "+str(preds[1]*100))
+#     
+#     # 启动session 计算TF图
+#     with tf.Session() as sess:
+#         np.random.seed(1)
+#         tf.set_random_seed(1)
+#         # 初使化
+#         init = tf.global_variables_initializer()
+#         sess.run(init)
+#         print("W1=", W1.eval()[1, 1, 1])  # 测试一下输出值
+#         print("W2=", W2.eval()[1, 1, 1])
+#         a = sess.run(Z3, {X: np.random.randn(2, 64, 64, 3), Y: np.random.randn(2, 6)})
+#         print("Z3 = " + str(a))
+#         a = sess.run(costJ, {X: np.random.randn(4, 64, 64, 3), Y: np.random.randn(4, 6)})
+#         print("cost = " + str(a))
+#     
+#         for epoch in range(num_epochs):  # 循环迭代次数
+#             batchcost = 0
+#             seed = seed + 1
+#             minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+#             # 分批计算
+#             for batch in minibatches:
+#                 (batch_X, batch_Y) = batch
+#                 _, cost = sess.run([optimizer, costJ], feed_dict={X:batch_X, Y:batch_Y})
+#                 batchcost += cost / int(m / minibatch_size)
+#              
+#             if epoch % 5 == 0:
+#                 print ("Cost after epoch %i: %f" % (epoch, batchcost))
+#             costs.append(batchcost)   
+#     
+#     plt.plot(np.squeeze(costs))
+#     plt.ylabel('cost')
+#     plt.xlabel('iterations (per tens)')
+#     plt.title("Learning rate =" + str(learning_rate))
+#     plt.show()
+#  
+#     # Calculate the correct predictions
+#     predict_op = tf.argmax(Z3, 1)
+#     correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+#  
+#     # Calculate accuracy on the test set
+#     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+#     print(accuracy)
+#     train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+#     test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+#     print("Train Accuracy:", train_accuracy)
+#     print("Test Accuracy:", test_accuracy)
             
