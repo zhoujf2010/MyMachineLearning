4e05304d5ef84d1dcacc82d18689aaeeabfc3278
zhoujf2010 <zhoujf2010@gmail.com>
2020-11-19 17:30:00 +0800

    音乐生成

--- chi_rnn/step5.py
+++ chi_rnn/step5.py
@@ -7,185 +7,192 @@ Created on 2019年2月10日
 
 '''
 用LSTM即兴创作Jazz
+#https://github.com/AdalbertoCq/Deep-Learning-Specialization-Coursera/tree/master/Sequence%20Models/week1/LSTM%20Network
 '''
 
-#https://github.com/AdalbertoCq/Deep-Learning-Specialization-Coursera/tree/master/Sequence%20Models/week1/LSTM%20Network
 
-import IPython
-import sys
-from music21 import *
+from music21 import stream, midi, tempo, note
 import numpy as np
-from music21 import *
-from collections import defaultdict, OrderedDict
-from itertools import groupby, zip_longest
 
-#from grammar import *
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from chi_rnn import step5_preprocess as pps
 
-#from grammar import parse_melody
-# from music_utils import *
+#----------------------------HELPER FUNCTIONS----------------------------------#
+
+''' Helper function to sample an index from a probability array '''
 
 
-def data_processing(corpus, values_indices, m = 60, Tx = 30):
-    # cut the corpus into semi-redundant sequences of Tx values
-    Tx = Tx 
+def __sample(a, temperature=1.0):
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+
+''' Helper function to generate a predicted value from a given matrix '''
+
+
+def __predict(model, x, indices_val, diversity):
+    preds = model.predict(x, verbose=0)[0]
+    next_index = __sample(preds, diversity)
+    next_val = indices_val[next_index]
+
+    return next_val
+
+''' Helper function which uses the given model to generate a grammar sequence 
+    from a given corpus, indices_val (mapping), abstract_grammars (list), 
+    and diversity floating point value. '''
+
+
+def __generate_grammar(model, corpus, abstract_grammars, values, val_indices,
+                       indices_val, max_len, max_tries, diversity):
+    curr_grammar = ''
+    # np.random.randint is exclusive to high
+    start_index = np.random.randint(0, len(corpus) - max_len)
+    sentence = corpus[start_index: start_index + max_len]  # seed
+    running_length = 0.0
+    while running_length <= 4.1:  # arbitrary, from avg in input file
+        # transform sentence (previous sequence) to matrix
+        x = np.zeros((1, max_len, len(values)))
+        for t, val in enumerate(sentence):
+            if (not val in val_indices): print(val)
+            x[0, t, val_indices[val]] = 1.
+
+        next_val = __predict(model, x, indices_val, diversity)
+
+        # fix first note: must not have < > and not be a rest
+        if (running_length < 0.00001):
+            tries = 0
+            while (next_val.split(',')[0] == 'R' or 
+                len(next_val.split(',')) != 2):
+                # give up after 1000 tries; random from input's first notes
+                if tries >= max_tries:
+                    print('Gave up on first note generation after', max_tries,
+                        'tries')
+                    # np.random is exclusive to high
+                    rand = np.random.randint(0, len(abstract_grammars))
+                    next_val = abstract_grammars[rand].split(' ')[0]
+                else:
+                    next_val = __predict(model, x, indices_val, diversity)
+
+                tries += 1
+
+        # shift sentence over with new value
+        sentence = sentence[1:] 
+        sentence.append(next_val)
+
+        # except for first case, add a ' ' separator
+        if (running_length > 0.00001): curr_grammar += ' '
+        curr_grammar += next_val
+
+        length = float(next_val.split(',')[1])
+        running_length += length
+
+    return curr_grammar
+
+
+def data_processing(corpus, tones_indices, max_len, step=3):
+#     number of different values or words in corpus
     N_values = len(set(corpus))
-    np.random.seed(0)
-    X = np.zeros((m, Tx, N_values), dtype=np.bool)
-    Y = np.zeros((m, Tx, N_values), dtype=np.bool)
-    for i in range(m):
-#         for t in range(1, Tx):
-        random_idx = np.random.choice(len(corpus) - Tx)
-        corp_data = corpus[random_idx:(random_idx + Tx)]
-        for j in range(Tx):
-            idx = values_indices[corp_data[j]]
-            if j != 0:
-                X[i, j, idx] = 1
-                Y[i, j-1, idx] = 1
-    
-    Y = np.swapaxes(Y,0,1)
-    Y = Y.tolist()
-    return np.asarray(X), np.asarray(Y), N_values 
-
-''' Helper function to parse a MIDI file into its measures and chords '''
-def __parse_midi(data_fn):
-    # Parse the MIDI data for separate melody and accompaniment parts.
-    midi_data = converter.parse(data_fn)
-    # Get melody part, compress into single voice.
-    melody_stream = midi_data[5]     # For Metheny piece, Melody is Part #5.
-    melody1, melody2 = melody_stream.getElementsByClass(stream.Voice)
-    for j in melody2:
-        melody1.insert(j.offset, j)
-    melody_voice = melody1
-
-    for i in melody_voice:
-        if i.quarterLength == 0.0:
-            i.quarterLength = 0.25
-
-    # Change key signature to adhere to comp_stream (1 sharp, mode = major).
-    # Also add Electric Guitar. 
-    melody_voice.insert(0, instrument.ElectricGuitar())
-    melody_voice.insert(0, key.KeySignature(sharps=1))
-
-    # The accompaniment parts. Take only the best subset of parts from
-    # the original data. Maybe add more parts, hand-add valid instruments.
-    # Should add least add a string part (for sparse solos).
-    # Verified are good parts: 0, 1, 6, 7 '''
-    partIndices = [0, 1, 6, 7]
-    comp_stream = stream.Voice()
-    comp_stream.append([j.flat for i, j in enumerate(midi_data) 
-        if i in partIndices])
-
-    # Full stream containing both the melody and the accompaniment. 
-    # All parts are flattened. 
-    full_stream = stream.Voice()
-    for i in range(len(comp_stream)):
-        full_stream.append(comp_stream[i])
-    full_stream.append(melody_voice)
-
-    # Extract solo stream, assuming you know the positions ..ByOffset(i, j).
-    # Note that for different instruments (with stream.flat), you NEED to use
-    # stream.Part(), not stream.Voice().
-    # Accompanied solo is in range [478, 548)
-    solo_stream = stream.Voice()
-    for part in full_stream:
-        curr_part = stream.Part()
-        curr_part.append(part.getElementsByClass(instrument.Instrument))
-        curr_part.append(part.getElementsByClass(tempo.MetronomeMark))
-        curr_part.append(part.getElementsByClass(key.KeySignature))
-        curr_part.append(part.getElementsByClass(meter.TimeSignature))
-        curr_part.append(part.getElementsByOffset(476, 548, 
-                                                  includeEndBoundary=True))
-        cp = curr_part.flat
-        solo_stream.insert(cp)
-
-    # Group by measure so you can classify. 
-    # Note that measure 0 is for the time signature, metronome, etc. which have
-    # an offset of 0.0.
-    melody_stream = solo_stream[-1]
-    measures = OrderedDict()
-    offsetTuples = [(int(n.offset / 4), n) for n in melody_stream]
-    measureNum = 0 # for now, don't use real m. nums (119, 120)
-    for key_x, group in groupby(offsetTuples, lambda x: x[0]):
-        measures[measureNum] = [n[1] for n in group]
-        measureNum += 1
-
-    # Get the stream of chords.
-    # offsetTuples_chords: group chords by measure number.
-    chordStream = solo_stream[0]
-    chordStream.removeByClass(note.Rest)
-    chordStream.removeByClass(note.Note)
-    offsetTuples_chords = [(int(n.offset / 4), n) for n in chordStream]
-
-    # Generate the chord structure. Use just track 1 (piano) since it is
-    # the only instrument that has chords. 
-    # Group into 4s, just like before. 
-    chords = OrderedDict()
-    measureNum = 0
-    for key_x, group in groupby(offsetTuples_chords, lambda x: x[0]):
-        chords[measureNum] = [n[1] for n in group]
-        measureNum += 1
-
-    # Fix for the below problem.
-    #   1) Find out why len(measures) != len(chords).
-    #   ANSWER: resolves at end but melody ends 1/16 before last measure so doesn't
-    #           actually show up, while the accompaniment's beat 1 right after does.
-    #           Actually on second thought: melody/comp start on Ab, and resolve to
-    #           the same key (Ab) so could actually just cut out last measure to loop.
-    #           Decided: just cut out the last measure. 
-    del chords[len(chords) - 1]
-    assert len(chords) == len(measures)
-
-    return measures, chords
-
-''' Helper function to get the grammatical data from given musical data. '''
-def __get_abstract_grammars(measures, chords):
-    # extract grammars
-    abstract_grammars = []
-    for ix in range(1, len(measures)):
-        m = stream.Voice()
-        for i in measures[ix]:
-            m.insert(i.offset, i)
-        c = stream.Voice()
-        for j in chords[ix]:
-            c.insert(j.offset, j)
-        parsed = parse_melody(m, c)
-        abstract_grammars.append(parsed)
-
-    return abstract_grammars
-
-''' Get musical data from a MIDI file '''
-def get_musical_data(data_fn):
-    
-    measures, chords = __parse_midi(data_fn)
-    abstract_grammars = __get_abstract_grammars(measures, chords)
-
-    return chords, abstract_grammars
-
-''' Get corpus data from grammatical data '''
-def get_corpus_data(abstract_grammars):
-    corpus = [x for sublist in abstract_grammars for x in sublist.split(' ')]
-    values = set(corpus)
-    val_indices = dict((v, i) for i, v in enumerate(values))
-    indices_val = dict((i, v) for i, v in enumerate(values))
-
-    return corpus, values, val_indices, indices_val
-
-
-def load_music_utils():
-    chords, abstract_grammars = get_musical_data('data/original_metheny.mid')
-    corpus, tones, tones_indices, indices_tones = get_corpus_data(abstract_grammars)
-    N_tones = len(set(corpus))
-    X, Y, N_tones = data_processing(corpus, tones_indices, 60, 30)   
-    return (X, Y, N_tones, indices_tones)
+ 
+    # cut the corpus into semi-redundant sequences of max_len values
+    sentences = []
+    next_values = []
+    for i in range(0, len(corpus) - max_len, step):
+        sentences.append(corpus[i: i + max_len])
+        next_values.append(corpus[i + max_len])
+    print('nb sequences:', len(sentences))
+ 
+    # transform data into binary matrices
+    X = np.zeros((len(sentences), max_len, N_values), dtype=np.bool)
+    y = np.zeros((len(sentences), N_values), dtype=np.bool)
+    for i, sentence in enumerate(sentences):
+        for t, val in enumerate(sentence):
+            X[i, t, tones_indices[val]] = 1
+        y[i, tones_indices[next_values[i]]] = 1
+    return X, y
 
 
 if __name__ == '__main__':
-#     IPython.display.Audio('./data/30s_seq.mp3')
-    X, Y, n_values, indices_values = load_music_utils()
+    # 加载midi数据
+    chords, abstract_grammars = pps.get_musical_data('data/original_metheny.mid')
+    corpus, tones, tones_indices, indices_tones = pps.get_corpus_data(abstract_grammars)
+    N_tones = len(set(corpus))
+    max_len = 20
+    X, Y = data_processing(corpus, tones_indices, max_len, 3)   
     print('shape of X:', X.shape)
     print('number of training examples:', X.shape[0])
     print('Tx (length of sequence):', X.shape[1])
-    print('total # of unique values:', n_values)
+    print('total # of unique values:', N_tones)
     print('Shape of Y:', Y.shape)
+
+    max_tries = 1000
+    diversity = 0.5
+    N_epochs = 2
     
+    # build model
+    model = Sequential()
+    model.add(LSTM(128, return_sequences=True, input_shape=(max_len, N_tones)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(128, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(N_tones))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.summary()
+
+    model.fit(X, Y, batch_size=128, epochs=N_epochs)
     
+    # set up audio stream
+    out_stream = stream.Stream()
+
+    # 循环生成
+    curr_offset = 0.0
+    for loopIndex in range(1, len(chords)):
+        # get chords from file
+        curr_chords = stream.Voice()
+        for j in chords[loopIndex]:
+            curr_chords.insert((j.offset % 4), j)
+
+        # generate grammar
+        curr_grammar = __generate_grammar(model=model, corpus=corpus,
+                                          abstract_grammars=abstract_grammars,
+                                          values=tones, val_indices=tones_indices,
+                                          indices_val=indices_tones,
+                                          max_len=max_len, max_tries=max_tries,
+                                          diversity=diversity)
+
+        curr_grammar = curr_grammar.replace(' A', ' C').replace(' X', ' C')
+        # Pruning #1: smoothing measure
+        curr_grammar = pps.prune_grammar(curr_grammar)
+        # Get notes from grammar and chords
+        curr_notes = pps.unparse_grammar(curr_grammar, curr_chords)
+        # Pruning #2: removing repeated and too close together notes
+        curr_notes = pps.prune_notes(curr_notes)
+        # quality assurance: clean up notes
+        curr_notes = pps.clean_up_notes(curr_notes)
+        # print # of notes in curr_notes
+        print('After pruning: %s notes' % (len([i for i in curr_notes
+            if isinstance(i, note.Note)])))
+
+        # insert into the output stream
+        for m in curr_notes:
+            out_stream.insert(curr_offset + m.offset, m)
+        for mc in curr_chords:
+            out_stream.insert(curr_offset + mc.offset, mc)
+
+        curr_offset += 4.0
+        
+    # musical settings
+    out_stream.insert(0.0, tempo.MetronomeMark(number=130))
+
+    #播放生成的音乐
+    midi.realtime.StreamPlayer(out_stream).play()
+
+    #保存文件
+#     mf = midi.translate.streamToMidiFile(out_stream)
+#     mf.open("out.mid", 'wb')
+#     mf.write()
+#     mf.close()
