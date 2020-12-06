b5ab2ba907b611553a1a2f4c9c584ff92cdf1e43
zhoujf2010 <zhoujf2010@gmail.com>
2020-12-06 14:33:01 +0800

    finish audio detect

--- chi_rnn/step7.py
+++ chi_rnn/step7.py
@@ -2,93 +2,98 @@
 Created on 2020年11月19日
 
 @author: zjf
+
+机器翻译示例（人类时间表达-》机器时间）
 '''
-from keras.layers import Bidirectional, Concatenate, Permute, Dot, Input, LSTM, Multiply
-from keras.layers import RepeatVector, Dense, Activation, Lambda
+import random
+
+from babel.dates import format_date
+from faker import Faker
+from keras.layers import Bidirectional, Concatenate, Dot, Input, LSTM
+from keras.layers import RepeatVector, Dense, Activation
+from keras.models import Model
 from keras.optimizers import Adam
 from keras.utils import to_categorical
-from keras.models import load_model, Model
+from tqdm import tqdm
+
 import keras.backend as K
 import numpy as np
 
-from faker import Faker
-import random
-from tqdm import tqdm
-from babel.dates import format_date
-from nmt_utils import *
-import matplotlib.pyplot as plt
 
-
-m = 10000
-dataset, human_vocab, machine_vocab, inv_machine_vocab = load_dataset(m)
-print(dataset[:10])
-Tx = 30
-Ty = 10
-X, Y, Xoh, Yoh = preprocess_data(dataset, human_vocab, machine_vocab, Tx, Ty)
-
-print("X.shape:", X.shape)
-print("Y.shape:", Y.shape)
-print("Xoh.shape:", Xoh.shape)
-print("Yoh.shape:", Yoh.shape)
-
-index = 0
-print("Source date:", dataset[index][0])
-print("Target date:", dataset[index][1])
-print()
-print("Source after preprocessing (indices):", X[index])
-print("Target after preprocessing (indices):", Y[index])
-print()
-print("Source after preprocessing (one-hot):", Xoh[index])
-print("Target after preprocessing (one-hot):", Yoh[index])
-
-
-# Defined shared layers as global variables
-repeator = RepeatVector(Tx)
-concatenator = Concatenate(axis=-1)
-densor1 = Dense(10, activation = "tanh")
-densor2 = Dense(1, activation = "relu")
-activator = Activation(softmax, name='attention_weights') # We are using a custom softmax(axis = 1) loaded in this notebook
-dotor = Dot(axes = 1)
-
-
-# GRADED FUNCTION: one_step_attention
-
-def one_step_attention(a, s_prev):
+# from nmt_utils import *
+def load_dataset(m):
     """
-    Performs one step of attention: Outputs a context vector computed as a dot product of the attention weights
-    "alphas" and the hidden states "a" of the Bi-LSTM.
-
-    Arguments:
-    a -- hidden state output of the Bi-LSTM, numpy-array of shape (m, Tx, 2*n_a)
-    s_prev -- previous hidden state of the (post-attention) LSTM, numpy-array of shape (m, n_s)
-
-    Returns:
-    context -- context vector, input of the next (post-attetion) LSTM cell
+       生成训练数据[[人类时间，计算机时间]]
     """
 
-    ### START CODE HERE ###
-    # Use repeator to repeat s_prev to be of shape (m, Tx, n_s) so that you can concatenate it with all hidden states "a" (≈ 1 line)
-    s_prev = repeator(s_prev)
-    # Use concatenator to concatenate a and s_prev on the last axis (≈ 1 line)
-    concat = concatenator([a, s_prev])
-    # Use densor1 to propagate concat through a small fully-connected neural network to compute the "intermediate energies" variable e. (≈1 lines)
-    e = densor1(concat)
-    # Use densor2 to propagate e through a small fully-connected neural network to compute the "energies" variable energies. (≈1 lines)
-    energies = densor2(e)
-    # Use "activator" on "energies" to compute the attention weights "alphas" (≈ 1 line)
-    alphas = activator(energies)
-    # Use dotor together with "alphas" and "a" to compute the context vector to be given to the next (post-attention) LSTM-cell (≈ 1 line)
-    context = dotor([alphas, a])
-    ### END CODE HERE ###
+    FORMATS = ['short', 'medium', 'long', 'full''d MMM YYY', 'd MMMM YYY', 'dd MMM YYY', 'd MM YY',
+               'd MMMM YYY', 'MMMM d YYY', 'MMMM d, YYY', 'dd.MM.YY']
+    
+    fake = Faker()
+    Faker.seed(1)
+    random.seed(1)
 
-    return context
+    dataset = []
+    h_vocab = set()  # 记录出现的字符
+    m_vocab = set()  # 记录出现的字符
+    for _ in tqdm(range(m)):
+        dt = fake.date_object()  # 随机生成一个日期数据
+        h = format_date(dt, format=random.choice(FORMATS), locale='en_US')  # locale=random.choice(LOCALES))
+        h = h.lower()
+        m = dt.isoformat()
+        dataset.append((h, m))
+        
+        h_vocab.update(tuple(h))
+        m_vocab.update(tuple(m))
+    return dataset, h_vocab, m_vocab
 
-n_a = 32
-n_s = 64
-post_activation_LSTM_cell = LSTM(n_s, return_state = True)
-output_layer = Dense(len(machine_vocab), activation=softmax)
 
-def model(Tx, Ty, n_a, n_s, human_vocab_size, machine_vocab_size):
+def string_to_int(string, length, vocab):
+    string = string.lower()
+    if len(string) > length:
+        string = string[:length]
+
+    rep = list(map(lambda x: vocab.get(x, '<unk>'), string))
+
+    if len(string) < length:
+        rep += [vocab['<pad>']] * (length - len(string))
+    return rep
+
+
+def preprocess_data(dataset, h_vocab, m_vocab, Tx, Ty):
+    X, Y = zip(*dataset)
+
+    X = np.array([string_to_int(i, Tx, h_vocab) for i in X])
+    Y = [string_to_int(t, Ty, m_vocab) for t in Y]
+
+    Xoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(h_vocab)), X)))
+    Yoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(m_vocab)), Y)))
+
+    return Xoh, Yoh
+
+
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
+ 
+ 
+def model(Tx, Ty, n_a, n_s, h_vocab_size, m_vocab_size):
     """
     Arguments:
     Tx -- length of the input sequence
@@ -97,74 +102,112 @@ def model(Tx, Ty, n_a, n_s, human_vocab_size, machine_vocab_size):
     n_s -- hidden state size of the post-attention LSTM
     human_vocab_size -- size of the python dictionary "human_vocab"
     machine_vocab_size -- size of the python dictionary "machine_vocab"
-
+ 
     Returns:
     model -- Keras model instance
     """
-
+ 
     # Define the inputs of your model with a shape (Tx,)
     # Define s0 and c0, initial hidden state for the decoder LSTM of shape (n_s,)
-    X = Input(shape=(Tx, human_vocab_size))
+    X = Input(shape=(Tx, h_vocab_size))
     s0 = Input(shape=(n_s,), name='s0')
     c0 = Input(shape=(n_s,), name='c0')
     s = s0
     c = c0
-
+ 
+    # Defined shared layers as global variables
+    repeator = RepeatVector(Tx)
+    concatenator = Concatenate(axis=-1)
+    densor1 = Dense(10, activation="tanh")
+    densor2 = Dense(1, activation="relu")
+    activator = Activation(softmax, name='attention_weights')  # We are using a custom softmax(axis = 1) loaded in this notebook
+    dotor = Dot(axes=1)
+    
+    post_activation_LSTM_cell = LSTM(n_s, return_state=True)
+    output_layer = Dense(m_vocab_size, activation=softmax)
+ 
     # Initialize empty list of outputs
     outputs = []
-
-    ### START CODE HERE ###
-
+ 
     # Step 1: Define your pre-attention Bi-LSTM. Remember to use return_sequences=True. (≈ 1 line)
     a = Bidirectional(LSTM(n_a, return_sequences=True))(X)
-
+ 
     # Step 2: Iterate for Ty steps
-    for t in range(Ty):
-
+    for _ in range(Ty):
+ 
         # Step 2.A: Perform one step of the attention mechanism to get back the context vector at step t (≈ 1 line)
-        context = one_step_attention(a, s)
-
+        # Use repeator to repeat s_prev to be of shape (m, Tx, n_s) so that you can concatenate it with all hidden states "a" (≈ 1 line)
+        s_prev = repeator(s)
+        # Use concatenator to concatenate a and s_prev on the last axis (≈ 1 line)
+        concat = concatenator([a, s_prev])
+        # Use densor1 to propagate concat through a small fully-connected neural network to compute the "intermediate energies" variable e. (≈1 lines)
+        e = densor1(concat)
+        # Use densor2 to propagate e through a small fully-connected neural network to compute the "energies" variable energies. (≈1 lines)
+        energies = densor2(e)
+        # Use "activator" on "energies" to compute the attention weights "alphas" (≈ 1 line)
+        alphas = activator(energies)
+        # Use dotor together with "alphas" and "a" to compute the context vector to be given to the next (post-attention) LSTM-cell (≈ 1 line)
+        context = dotor([alphas, a])
+ 
         # Step 2.B: Apply the post-attention LSTM cell to the "context" vector.
         # Don't forget to pass: initial_state = [hidden state, cell state] (≈ 1 line)
-        s, _, c = post_activation_LSTM_cell(context, initial_state = [s, c])
-
+        s, _, c = post_activation_LSTM_cell(context, initial_state=[s, c])
+ 
         # Step 2.C: Apply Dense layer to the hidden state output of the post-attention LSTM (≈ 1 line)
         out = output_layer(s)
-
+ 
         # Step 2.D: Append "out" to the "outputs" list (≈ 1 line)
         outputs.append(out)
-
+ 
     # Step 3: Create model instance taking three inputs and returning the list of outputs. (≈ 1 line)
-    model = Model(inputs=[X,s0,c0],outputs=outputs)
-
-    ### END CODE HERE ###
-
+    model = Model(inputs=[X, s0, c0], outputs=outputs)
     return model
+ 
 
-model = model(Tx, Ty, n_a, n_s, len(human_vocab), len(machine_vocab))
-
-model.summary()
-
-### START CODE HERE ### (≈2 lines)
-opt = Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, decay=0.01)
-model.compile(loss = 'categorical_crossentropy',optimizer=opt, metrics = ['accuracy'])
-### END CODE HERE ###
-
-s0 = np.zeros((m, n_s))
-c0 = np.zeros((m, n_s))
-outputs = list(Yoh.swapaxes(0,1))
-
-model.fit([Xoh, s0, c0], outputs, epochs=1, batch_size=100)
-
-EXAMPLES = ['3 May 1979', '5 April 09', '21th of August 2016', 'Tue 10 Jul 2007', 'Saturday May 9 2018', 'March 3 2001', 'March 3rd 2001', '1 March 2001']
-for example in EXAMPLES:
-
-    source = string_to_int(example, Tx, human_vocab)
-    source = np.array(list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), source))).swapaxes(0,1)
-    prediction = model.predict([source, s0, c0])
-    prediction = np.argmax(prediction, axis = -1)
-    output = [inv_machine_vocab[int(i)] for i in prediction]
-
-    print("source:", example)
-    print("output:", ''.join(output))
+if __name__ == '__main__':
+    m = 100
+    # 生成数据
+    dataset, h_vocab, m_vocab = load_dataset(m)
+    # 提取字典
+    h_vocab = dict(zip(sorted(h_vocab) + ['<unk>', '<pad>'], list(range(len(h_vocab) + 2))))
+    m_vocab = dict(zip(sorted(m_vocab), list(range(len(m_vocab)))))
+    inv_m_vocab = {v:k for k, v in m_vocab.items()}
+    print('february 22, 1979', '---->', string_to_int('february 22, 1979', 30, h_vocab))
+    print('1979-02-22', '---->', string_to_int('1979-02-22', 10, m_vocab))
     
+    Tx = 30  # 输入最长30字符
+    Ty = 10  # 输出最长10字符
+    Xoh, Yoh = preprocess_data(dataset, h_vocab, m_vocab, Tx, Ty)
+    
+    print(dataset[:10])
+    print("Source date:", dataset[1][0])
+    print("Target date:", dataset[1][1])
+    print("Source after preprocessing (one-hot):", Xoh[1])
+    print("Target after preprocessing (one-hot):", Yoh[1])
+    
+    #定义模型
+    n_a = 32
+    n_s = 64
+    model = model(Tx, Ty, n_a, n_s, len(h_vocab), len(m_vocab))
+    opt = Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, decay=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    model.summary()
+    
+    #模型训练 
+    outputs = list(Yoh.swapaxes(0, 1))
+    model.fit([Xoh, np.zeros((m, n_s)), np.zeros((m, n_s))], outputs, epochs=2, batch_size=1000)
+     
+    #模型验证
+    EXAMPLES = ['9 may 1998', '3 May 1979', '5 April 09', '21th of August 2016', 'Tue 10 Jul 2007', 'Saturday May 9 2018', 'March 3 2001', 'March 3rd 2001', '1 March 2001']
+    for example in EXAMPLES:
+     
+        source = string_to_int(example, Tx, h_vocab)
+        source = np.array(list(map(lambda x: to_categorical(x, num_classes=len(h_vocab)), source))).swapaxes(0, 1).T
+    
+        prediction = model.predict([source.reshape([1, 30, 37]), np.zeros((1, n_s)), np.zeros((1, n_s))])
+        prediction = np.argmax(prediction, axis=-1)
+        output = [inv_m_vocab[int(i)] for i in prediction]
+     
+        print("source:", example)
+        print("output:", ''.join(output))
+         
--- chi_rnn/step8.py
+++ chi_rnn/step8.py
@@ -4,7 +4,280 @@ Created on 2020年11月19日
 @author: zjf
 '''
 import numpy as np
-from pydub import AudioSegment      # pip install, 处理音频的函数库
-import random
-import sys
-import io
+from scipy.io import wavfile
+import simpleaudio as sa
+
+from keras.models import Model, load_model
+from keras.layers import Dense, Activation, Dropout, Input, TimeDistributed, Conv1D
+from keras.layers import GRU, BatchNormalization
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+
+def insert_audio_clip(background, audio_clip, pos):
+    """
+    在pos处，将audio插入到background中
+    """
+    p = len(audio_clip)
+    newdt = np.vstack((background[:pos] , audio_clip , background[pos + p:]))
+    return newdt
+
+
+def get_random_time_segment(cliplen, previous_segments):
+    """
+        取得一个随机位置,cliplen表示要插入的长度，防止到最尾头
+    """
+    while True:
+        segment_start = np.random.randint(low=0, high=441000 - cliplen)  # 441000为10秒的背景音乐的长度，不能超过 
+        segment_end = segment_start + cliplen - 1
+        
+        overlap = False
+        for previous_start, previous_end in previous_segments:
+            if segment_start <= previous_end and segment_end >= previous_start:
+                overlap = True
+        if not overlap:
+            break
+    previous_segments.append((segment_start, segment_end))
+    return (segment_start, segment_end)
+
+
+def calcSpectral(x, nfft, fs, stride):
+    '''
+    计算频谱信息
+    '''
+    windownum = (x.shape[-1] - nfft + stride) // stride
+    numFreqs = nfft // 2 + 1
+    
+    # 滑动取出x的值
+    strides = (x.strides[0], stride * x.strides[0])
+    result = np.lib.stride_tricks.as_strided(x, shape=(nfft, windownum), strides=strides)
+
+    # 滑动xxx
+    tmp = np.ones(nfft, dtype=x.dtype)
+    xt = np.hanning(nfft) * tmp
+    strides = (xt.strides[0], 0)
+    windowValsRep = np.lib.stride_tricks.as_strided(xt, shape=(nfft, windownum), strides=strides)  
+    result = windowValsRep * result
+    
+    result = np.fft.fft(result, n=nfft, axis=0)[:numFreqs, :]
+    result = np.conj(result) * result
+    result = result.real
+    slc = slice(1, -1, None)
+    result[slc] *= 2
+    result /= fs
+    result /= (np.abs(xt) ** 2).sum()
+    
+    return result
+
+
+def create_onetraining_example(backgrounds, activates, negatives, Ty):
+    """
+    创建一条训练数据
+    """
+    y = np.zeros((1, Ty))  # 定义输出y
+ 
+    data = backgrounds[np.random.randint(0, len(backgrounds))]
+    previous_segments = []
+ 
+    # 随机挑选0~4个正样本，插入到背景
+    random_indices = np.random.randint(len(activates), size=np.random.randint(1, 5))
+    for index in random_indices:
+        # 随机位置，并插入背景声音中
+        insertpos = get_random_time_segment(len(activates[index]), previous_segments)
+        data = insert_audio_clip(data, activates[index], insertpos[0])
+        
+        # 将插入点的尾部，记录y值为1
+        segment_end_y = insertpos[1] * Ty // 441000
+        for i in range(segment_end_y + 1, min(segment_end_y + 51, Ty)):
+            y[0, i] = 1.0
+ 
+    # 随机挑选0~2个负样本插入到背景中
+    random_indices = np.random.randint(len(negatives), size=np.random.randint(0, 3))
+    for index in random_indices:
+        # 随机位置，并插入背景声音中
+        insertpos = get_random_time_segment(len(negatives[index]), previous_segments)
+        data = insert_audio_clip(data, negatives[index], insertpos[0])
+ 
+    # 取得频域数据
+    data = calcSpectral(data[:, 0], nfft, fs, stride)
+ 
+    return data.T, y.T
+
+
+def model(input_shape):
+    """
+    Function creating the model's graph in Keras.
+ 
+    Argument:
+    input_shape -- shape of the model's input data (using Keras conventions)
+ 
+    Returns:
+    model -- Keras model instance
+    """
+ 
+    X_input = Input(shape=input_shape)
+ 
+    ### START CODE HERE ###
+ 
+    # Step 1: CONV layer (≈4 lines)
+    X = Conv1D(196, 15, strides=4)(X_input)  # CONV1D
+    X = BatchNormalization()(X)  # Batch normalization
+    X = Activation('relu')(X)  # ReLu activation
+    X = Dropout(0.8)(X)  # dropout (use 0.8)
+ 
+    # Step 2: First GRU Layer (≈4 lines)
+    X = GRU(units=128, return_sequences=True, reset_after=True)(X)  # GRU (use 128 units and return the sequences)
+    X = Dropout(0.8)(X)  # dropout (use 0.8)
+    X = BatchNormalization()(X)  # Batch normalization
+ 
+    # Step 3: Second GRU Layer (≈4 lines)
+    X = GRU(units=128, return_sequences=True, reset_after=True)(X)  # GRU (use 128 units and return the sequences)
+    X = Dropout(0.8)(X)  # dropout (use 0.8)
+    X = BatchNormalization()(X)  # Batch normalization
+    X = Dropout(0.8)(X)  # dropout (use 0.8)
+ 
+    # Step 4: Time-distributed dense layer (≈1 line)
+    X = TimeDistributed(Dense(1, activation="sigmoid"))(X)  # time distributed  (sigmoid)
+ 
+    ### END CODE HERE ###
+ 
+    model = Model(inputs=X_input, outputs=X)
+ 
+    return model  
+
+
+def detect_triggerword(filename, model):
+    
+    _, audio = wavfile.read(filename)
+    audio_spec = calcSpectral(audio[:, 0], nfft, fs, stride)
+     
+    ax1 = plt.subplot(2, 1, 1)
+    ax1.imshow(np.flipud(10. * np.log10(audio_spec)))
+    ax1.axis('auto')
+    
+    predictions = model.predict(np.array([audio_spec.T]))
+ 
+    ax2 = plt.subplot(2, 1, 2)
+    ax2.plot(predictions[0, :, 0])
+#     ax2.ylabel('probability')
+    
+    plt.show()
+
+# chime_file = "data/soundData/audio_examples/chime.wav"
+# def chime_on_activate(filename, predictions, threshold):
+#     audio_clip = AudioSegment.from_wav(filename)
+#     chime = AudioSegment.from_wav(chime_file)
+#     Ty = predictions.shape[1]
+#     # Step 1: Initialize the number of consecutive output steps to 0
+#     consecutive_timesteps = 0
+#     # Step 2: Loop over the output steps in the y
+#     for i in range(Ty):
+#         # Step 3: Increment consecutive output steps
+#         consecutive_timesteps += 1
+#         # Step 4: If prediction is higher than the threshold and more than 75 consecutive output steps have passed
+#         if predictions[0,i,0] > threshold and consecutive_timesteps > 75:
+#             # Step 5: Superpose audio and background using pydub
+#             audio_clip = audio_clip.overlay(chime, position = ((i / Ty) * audio_clip.duration_seconds)*1000)
+#             # Step 6: Reset consecutive output steps to 0
+#             consecutive_timesteps = 0
+#  
+#     audio_clip.export("chime_output.wav", format='wav')
+
+if __name__ == '__main__':
+    np.random.seed(5)
+
+    rate, audio = wavfile.read("./data/soundData/activates/1.wav")
+    print('rate=', rate, 'len=', np.shape(audio))
+#     sa.play_buffer(audio, 2, 2, rate).wait_done()
+    _, background = wavfile.read("./data/soundData/backgrounds/1.wav")
+    
+    newdt = insert_audio_clip(background, audio, 220000)
+    print(np.shape(newdt))
+#     sa.play_buffer(newdt, 2, 2, 44100).wait_done()
+    # wavfile.write("test.wav", 44100, newdt)
+    previous_segments = []
+    p1 = get_random_time_segment(770, previous_segments)
+    print(p1)
+    p2 = get_random_time_segment(770, previous_segments)
+    print(p2)
+
+    nfft = 200  # 每个窗口段的大小
+    stride = 80  # 窗口滑动步幅
+    fs = 8000  # Sampling frequencies
+
+    Tx = 5511  # (441000-200+80)/80 The number of time steps input to the model from the spectrogram
+    n_freq = 101  # Number of frequencies input to the model at each time step of the spectrogram
+    Ty = 1375  # The number of time steps in the output of our model
+    
+    print(np.shape(background))
+    background_spec = calcSpectral(background[:, 0], nfft, fs, stride)
+    print(np.shape(background_spec))
+    newdt_spec = calcSpectral(newdt[:, 0], nfft, fs, stride)
+    audio_spec = calcSpectral(audio[:, 0], nfft, fs, stride)
+     
+#     ax1 = plt.subplot(2, 2, 1)
+#     ax1.imshow(np.flipud(10. * np.log10(background_spec)))
+#     ax1.axis('auto')
+#     ax2 = plt.subplot(2, 2, 2)
+#     ax2.imshow(np.flipud(10. * np.log10(audio_spec)))
+#     ax2.axis('auto')
+#     ax3 = plt.subplot(2, 1, 2)
+#     ax3.imshow(np.flipud(10. * np.log10(newdt_spec)))
+#     ax3.axis('auto')
+     
+#     plt.show()
+
+    # 加载样本数据
+    filepath = "./data/soundData"
+    activates = [wavfile.read("%s/activates/%d.wav" % (filepath, i))[1] for i in range(0, 9)]
+    backgrounds = [wavfile.read("%s/backgrounds/%d.wav" % (filepath, i))[1] for i in range(0, 1)]
+    negatives = [wavfile.read("%s/negatives/%d.wav" % (filepath, i))[1] for i in range(0, 9)]
+    
+    # 创建一条数据
+    x, y = create_onetraining_example(backgrounds, activates, negatives, Ty)
+    
+    print("shape(y)=", np.shape(x))
+    print("shape(y)=", np.shape(y))
+    
+    # 创建多条训练数据
+    X = []
+    Y = []
+    for _ in range(26):
+        x, y = create_onetraining_example(backgrounds, activates, negatives, Ty)
+        X.append(x)
+        Y.append(y)
+    
+    X = np.array(X)
+    Y = np.array(Y)
+    print(np.shape(X))
+    print(np.shape(Y))
+     
+    model = model(input_shape=(Tx, n_freq))
+    opt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, decay=0.01)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=["accuracy"])
+    model.summary()
+    
+    model = load_model('./data/tr_model.h5')
+
+#     model.fit(X, Y, batch_size=5, epochs=1)
+#     model.save("./data/tr_model.h5");
+    
+    # 创建测试数据并验证
+    X_dev = []
+    Y_dev = []
+    for _ in range(5):
+        x, y = create_onetraining_example(backgrounds, activates, negatives, Ty)
+        X_dev.append(x)
+        Y_dev.append(y)
+    
+    X_dev = np.array(X_dev)
+    Y_dev = np.array(Y_dev)
+    loss, acc = model.evaluate(X_dev, Y_dev)
+    print("Dev set accuracy = ", acc)
+    
+    predictions = model.predict(np.array([X_dev[0]]))
+    
+
+    detect_triggerword("data/soundData/dev/1.wav", model)
+#     chime_on_activate(filename, prediction, 0.5)
+  
